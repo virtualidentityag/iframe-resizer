87a9ab1a8b4ffcd46b673addd3bf7c914fbc04c7
Jonas Begemann <jonas.begemann@virtual-identity.com>
2015-12-18 15:29:23 +0100

    added new height calculation method which takes care of horizontal scrollbar height

--- js/iframeResizer.contentWindow.js
+++ js/iframeResizer.contentWindow.js
@@ -778,6 +778,16 @@
 
 			taggedElement: function getTaggedElementsHeight(){
 				return getTaggedElements('bottom','data-iframe-height');
+			},
+
+			elementOffsetWithScrollbar: function getElementOffsetWithScrollbarHeight(){
+				// if horizontal scrollbar exists add dimensions of scrollbar to calculated height
+				if(document.documentElement.scrollWidth > document.documentElement.offsetWidth) {
+					var scrollBarWidth = getScrollBarWidth();
+					return this.documentElementOffset() + scrollBarWidth;
+				} else {
+					return this.documentElementOffset();
+				}
 			}
 		},
 
@@ -818,6 +828,32 @@
 				return getTaggedElements('right', 'data-iframe-width');
 			}
 		};
+
+	function getScrollBarWidth () {
+		var inner = document.createElement('p');
+		inner.style.width = "100%";
+		inner.style.height = "200px";
+
+		var outer = document.createElement('div');
+		outer.style.position = "absolute";
+		outer.style.top = "0px";
+		outer.style.left = "0px";
+		outer.style.visibility = "hidden";
+		outer.style.width = "200px";
+		outer.style.height = "150px";
+		outer.style.overflow = "hidden";
+		outer.appendChild (inner);
+
+		document.body.appendChild (outer);
+		var w1 = inner.offsetWidth;
+		outer.style.overflow = 'scroll';
+		var w2 = inner.offsetWidth;
+		if (w1 == w2) w2 = outer.clientWidth;
+
+		document.body.removeChild (outer);
+
+		return (w1 - w2);
+	}
 
 
 	function sizeIFrame(triggerEvent, triggerEventDesc, customHeight, customWidth){
